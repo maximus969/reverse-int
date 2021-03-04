1d3878f4c151387513452b4cf60c157c8a74af79
Maxim Bordea <maxbordea@gmail.com>
2021-03-04 17:15:28 +0200

    feat: implement logic reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const reverseNumber = (n, acc) => 
+        n === 0 ? acc : reverseNumber(Math.floor(n / 10), acc * 10 + n % 10)
+    return reverseNumber(Math.abs(n), 0)
 }
